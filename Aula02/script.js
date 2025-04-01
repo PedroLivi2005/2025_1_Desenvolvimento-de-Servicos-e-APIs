7a8826a022826fc8d0991812255f42125488bf8d
Pedro Livi <pedro.livi2005@gmail.com>
2025-04-01 09:45:02 -0300

    Commit inicial, com as Aulas de 1 a 4

---
+++ Aula02/script.js
@@ -0,0 +1,11 @@
+
+$("#minhaDiv").html( "Olá <i>Pessoal</i>")
+//$("#minhaDiv").hide()
+
+//$("#botao").click( function(){
+//    $("#minhaDiv").toggle()
+//})
+
+$("#botao").on("click", function(){
+    $("#minhaDiv").toggle()
+})
